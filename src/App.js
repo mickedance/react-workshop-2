5019a3dec353ffeb550ca8cce7d489592daaf982
mickedance <mickedance@hotmail.com>
2022-09-30 13:36:53 +0200

    app modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {  Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 import DemoRouter from './DemoRouter';
+import Header from './Header';
 
 const App = () => {
     return (
